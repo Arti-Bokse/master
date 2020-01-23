a8abcd844494362f97f322f5bf05884ea328b2f2
Arti-Bokse <artibokse@gmail.com>
2020-01-23 16:41:18 +0530

    registration design

--- CourseCoordinator/app/src/main/java/arti/cdac/project/coursecoordinator/activity/RegistrationActivity.java
+++ CourseCoordinator/app/src/main/java/arti/cdac/project/coursecoordinator/activity/RegistrationActivity.java
@@ -2,7 +2,13 @@ package arti.cdac.project.coursecoordinator.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+import java.io.File;
 
 import arti.cdac.project.coursecoordinator.R;
 
@@ -12,5 +18,11 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void onUpload(View v) {
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
     }
 }
